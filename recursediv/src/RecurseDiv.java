dc1a9fa798471ba2d15a6a4b0c4c0d9a16193f4d
Tim Miller <neoparasak@gmail.com>
2015-04-21 12:28:36 -0400

    No message

--- recursediv/src/RecurseDiv.java
+++ recursediv/src/RecurseDiv.java
@@ -1,9 +1,43 @@
-
-
 import java.util.Scanner;
 
+/**
+ * RecurseDiv.java<br>
+ * Apr 21, 2015
+ * 
+ * @author Tim Miller
+ */
 public class RecurseDiv
 {
+	public static void main(String[] args)
+	{
+		boolean running = true;
+		final Scanner scan = new Scanner(System.in);
+		do
+		{
+			System.out.println("Please enter a dividend");
+			final int x = scan.nextInt();
+			System.out.println("Please enter a divisor");
+			final int y = scan.nextInt();
+			try
+			{
+				System.out.println(RecurseDiv.recursediv(x, y));
+			}
+			catch (final Exception e)
+			{
+				System.out
+				.println("An error occurred, please see stacktrace for details");
+				e.printStackTrace();
+			}
+			System.out
+			.println("Would you like to go again ('q' to  quit, else continue)");
+			scan.nextLine();
+			running = !scan.nextLine().equalsIgnoreCase("q");
+		}
+		while (running);
+		scan.close();
+		System.out.println("Goodbye!");
+	}
+
 	public static int recursediv(int a, int b)
 	{
 		if (a == b)
@@ -18,35 +52,5 @@ public class RecurseDiv
 		{
 			return (1 + RecurseDiv.recursediv(a - b, b));
 		}
-	}
-
-	public static void main(String[] args)
-	{
-		boolean running = true;
-		final Scanner scan = new Scanner(System.in);
-		do
-		{
-			System.out.println("Please enter a dividend");
-			final int x = scan.nextInt();
-			System.out.println("Please enter a divisor");
-			final int y = scan.nextInt();
-			try
-			{
-				System.out.println(recursediv(x, y));
-			}
-			catch (final Exception e)
-			{
-				System.out
-						.println("An error occurred, please see stacktrace for details");
-				e.printStackTrace();
-			}
-			System.out
-					.println("Would you like to go again ('q' to  quit, else continue)");
-			scan.nextLine();
-			running = !scan.nextLine().equalsIgnoreCase("q");
-		}
-		while (running);
-		scan.close();
-		System.out.println("Goodbye!");
 	}
 }
